cc8c306f4ebff6709120dbb251127166db6c7c5c
diesel09 <37728951+diesel09@users.noreply.github.com>
2022-02-03 14:07:39 -0500

    Update VPS-MX.sh

--- VPS-MX.sh
+++ VPS-MX.sh
@@ -504,10 +504,10 @@ REQUEST=$(ofus "$Key"|cut -d'/' -f2)
 [[ ! -d ${SCPinstal} ]] && mkdir ${SCPinstal}
 pontos="."
 stopping="Configurando Directorios"
-for arqx in $(cat $HOME/lista-arq); do
-msg -verm "${stopping}${pontos}"
-wget --no-check-certificate -O ${SCPinstal}/${arqx} ${IP}:81/${REQUEST}/${arqx} > /dev/null 2>&1 && verificar_arq "${arqx}" || error_fun
-tput cuu1 && tput dl1
+#for arqx in $(cat $HOME/lista-arq); do
+#msg -verm "${stopping}${pontos}"
+#wget --no-check-certificate -O ${SCPinstal}/${arqx} ${IP}:81/${REQUEST}/${arqx} > /dev/null 2>&1 && verificar_arq "${arqx}" || error_fun
+#tput cuu1 && tput dl1
 pontos+="."
 done
 sleep 1s
